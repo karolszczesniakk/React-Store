dd5ef2c534f957abbb50b997bdc3d694695ca895
Karol Szczesniak <karolszczesniakk@gmail.com>
2022-04-09 21:27:00 +0200

    add social media links to contact page

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,6 +3,7 @@ import Button from '../UI/Button';
 import Card from '../UI/Card';
 import { useNavigate } from 'react-router-dom';
 import classes from './Contact.module.css'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const About: React.FC = () => {
   const navigate = useNavigate();
@@ -25,18 +26,32 @@ const About: React.FC = () => {
         enim et, feugiat molestie tellus. Integer tempor ex nec nisl tempus, ut
         auctor ante auctor. Praesent mattis at tellus at consequat.
       </div>
-      <Card>
-        <form className={classes["contact-form"]} onSubmit={handleSubmit}>
-          <h1 className={classes["contact-form-title-text"]}>Contact us</h1>
-          <p>Your Name</p>
-          <input type="text" placeholder="Tony Stark" required></input>
-          <p>Your email</p>
-          <input type="email" placeholder="example@email.com" required></input>
-          <p>Subject</p>
-          <textarea placeholder="Your message..." required></textarea>
-          <Button type="submit">Submit</Button>
-        </form>
-      </Card>
+      <div>
+        <Card>
+          <form className={classes["contact-form"]} onSubmit={handleSubmit}>
+            <h1 className={classes["contact-form-title-text"]}>Contact us</h1>
+            <p>Your Name</p>
+            <input type="text" placeholder="Tony Stark" required></input>
+            <p>Your email</p>
+            <input
+              type="email"
+              placeholder="example@email.com"
+              required
+            ></input>
+            <p>Subject</p>
+            <textarea placeholder="Your message..." required></textarea>
+            <Button type="submit">Submit</Button>
+          </form>
+        </Card>
+        <div className={classes["contact-form-links"]}>
+          <a href="https://github.com/karolszczesniakk">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/karol-szcze%C5%9Bniak-52607b234/">
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
