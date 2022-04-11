2cfe83e151d95e21b615830d408ac60e67bd390b
Karol Szczesniak <karolszczesniakk@gmail.com>
2022-04-11 23:07:30 +0200

    remove max-height and overflow-y properties from modal css class, add max-height and overlow-y properties to cart-products-list css class

--- src/components/cart/cart-products/CartProductsList.tsx
+++ src/components/cart/cart-products/CartProductsList.tsx
@@ -3,13 +3,15 @@ import { RootState } from "../../../store";
 import { CartItem } from "../../../types/CartItem";
 import CartProduct from "./CartProduct";
 
+import classes from "./CartProductsList.module.css";
+
 const CartProductsList: React.FC = () => {
   const cartData = useSelector((state: RootState) => state.cart);
   const totalAmount = useSelector((state: RootState) => state.cart.totalAmount);
   const { cartItems } = cartData;
   return (
     <>
-      <div>
+      <div className={classes["cart-products-list"]}>
         {cartItems.map((item: CartItem) => (
           <CartProduct
             key={item.itemData.id}
@@ -19,7 +21,7 @@ const CartProductsList: React.FC = () => {
           />
         ))}
       </div>
-      <h2>Total amount: €{totalAmount}</h2>
+      <h2 className={classes.amount}>Total amount: €{totalAmount}</h2>
     </>
   );
 }
